e5d1cde68a77eaeada61a90c3ace21561fede60a
Vincent  GUILLEMOT <vincent.guillemot@pasteur.fr>
2021-11-13 23:26:38 +0100

    ADD tutos

--- R/debuter.R
+++ R/debuter.R
@@ -1,4 +1,4 @@
 #' Package pour la formation "Débuter en R"
-#' Contient les vignettes (les slides de cours), le jeu de données "fruits" et les tutoriels. https://vguillemot.github.io/debuter/
+#' Contient les vignettes (les slides de cours), le jeu de données "fruits", le jeu de données "nutrimenu"  et les tutoriels. https://vguillemot.github.io/debuter/
 #' @keywords internal
 "_PACKAGE"
